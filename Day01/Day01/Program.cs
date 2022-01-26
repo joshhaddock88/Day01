cb9384889e3cb7efdfae6ce629fa1b121a1c1e6c
Joshua Haddock <joshhaddock88@gmail.com>
2022-01-25 21:43:58 -0800

    All the Errors and Miles methods written

--- Day01/Day01/Program.cs
+++ Day01/Day01/Program.cs
@@ -6,7 +6,46 @@ namespace Day01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Miles();
+            AllTheErrors();
+        }
+
+        private static void AllTheErrors()
+        {
+            int num = 13;
+            int denom = 0;
+            int result;
+            int[] numbers = { 22, 33, 44 };
+
+            try
+            {
+                result = num / denom;
+            }
+            catch( Exception e )
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public static void Miles()
+        {
+            try
+            {
+                int milesDriven, gallonsOfGas, mpg;
+
+                Console.WriteLine("Enter the miles driven");
+                milesDriven = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("How many gallons of gas did you use?");
+                gallonsOfGas = Convert.ToInt32(Console.ReadLine());
+                mpg = milesDriven / gallonsOfGas;
+                Console.WriteLine($"You drove {milesDriven} on {gallonsOfGas}. Which gives you an MPG of {mpg}");
+            }
+            catch ( Exception e )
+            {
+                Console.WriteLine("You have entered a bad value");
+                Miles();
+            }
         }
     }
 }
